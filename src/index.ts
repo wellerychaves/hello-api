d7aa2a47dd749c625b1d878b0098fa429957da1b
wellerychaves <wellerychaves@outlook.com>
2024-07-25 15:20:26 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+async function fMizera() {
+	const baseURL = "https://panel.worldunited.gg/api/events/1400/best-times?filter=no_powerups&car=bmwm3gtre46";
+	const tracks = await fetch(baseURL)
+		.then((res) => res.json())
+		.then((res) => res.items)
+		.catch((err) => console.error(err));
+	return tracks;
+}
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+	.get("/", () => "Hello Elysia")
+	.get("/mizera", () => fMizera())
+	.listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
